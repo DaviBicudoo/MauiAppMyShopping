63930ff2a14b8334b93f163d72fd8a9f5f60e565
DAVI SILVA <davi.silva403@etec.sp.gov.br>
2024-07-23 15:55:29 -0300

    Development of "SQLiteDatabaseHelper.cs" class u sing CRUD pattern and SQLite 'library'

--- MauiAppMyShopping/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMyShopping/Helpers/SQLiteDatabaseHelper.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppMyShopping.Models;
+using SQLite;
+
 
 namespace MauiAppMyShopping.Helpers
 {
-    internal class SQLiteDatabaseHelper
+    public class SQLiteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _connection;
+
+        // Constructor
+        public SQLiteDatabaseHelper(string path) 
+        { 
+            _connection = new SQLiteAsyncConnection(path);
+            _connection.CreateTableAsync<Product>().Wait();
+        }
+
+        // Create method
+        public Task<int> Create(Product product) 
+        { 
+            return _connection.InsertAsync(product);
+        }
+
+        // Update method
+        public Task<List<Product>> Update(Product product) 
+        {
+            string SQL = "INSERT Product SET Description=?, Amount=?, Price=? WHERE Id=?";
+
+            // _connection.QueryAsync<Product>(SQL, product); --> "Easy way"
+
+            return _connection.QueryAsync<Product>(
+                SQL, product.Description, product.Amount, product.Price, product.Id
+                );
+        }
+
+        // Delete method
+        public Task<int> Delete(int id) 
+        {
+            return _connection.Table<Product>().DeleteAsync(i => i.Id == id);
+        }
+
+        // Getting all products
+        public Task<List<Product>> GetAll() 
+        {
+            return _connection.Table<Product>().ToListAsync();
+        }
+
+        // Search products
+        public Task<List<Product>> Search(string query) 
+        {
+            string SQL = $"SELECT * FROM Product WHERE Description LIKE '%{query}%' ";
+
+            return _connection.QueryAsync<Product>(SQL);  
+        }
     }
 }

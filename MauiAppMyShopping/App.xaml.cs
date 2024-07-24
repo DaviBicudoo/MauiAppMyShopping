b69109ebd8391c20844e57f85295c2e41887001e
DAVI SILVA <davi.silva403@etec.sp.gov.br>
2024-07-24 13:39:34 -0300

    Creating a "database" connection. Setting the path of '.sql' file and establishing connection.

--- MauiAppMyShopping/App.xaml.cs
+++ MauiAppMyShopping/App.xaml.cs
@@ -1,7 +1,28 @@
-﻿namespace MauiAppMyShopping
+﻿using MauiAppMyShopping.Helpers;
+
+namespace MauiAppMyShopping
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Database
+        {
+            get
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData), 
+                        "shopping_db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();

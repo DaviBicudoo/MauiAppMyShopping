130a63d3414a818053e0215f7fe6f399de3adfbb
Davi Bicudo <149242971+DaviBicudoo@users.noreply.github.com>
2025-03-24 10:30:54 -0300

    Update Product.cs

--- MauiAppMyShopping/Models/Product.cs
+++ MauiAppMyShopping/Models/Product.cs
@@ -4,15 +4,18 @@ namespace MauiAppMyShopping.Models
 {
     public class Product
     {
-        string _description;
+        string _description; 
 
         [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
+        // Product ID
+        public int Id { get; set; } 
+
+        // Product description
         public string? Description { 
             get => _description;
             set
             {
-                if(value == null)
+                if(value == null) // If there isn't any description, throw an exception
                 {
                     throw new Exception("Please, fill the fields");
                 }
@@ -20,8 +23,9 @@ namespace MauiAppMyShopping.Models
                 _description = value;
             }
         }
-        public int Amount { get; set; }
-        public double Price { get; set; }
-        public double Total { get => Convert.ToDouble(Amount) * Price; }
+        
+        public int Amount { get; set; } // Product amount
+        public double Price { get; set; } // Product price
+        public double Total { get => Convert.ToDouble(Amount) * Price; } // Product total price
     }
 }

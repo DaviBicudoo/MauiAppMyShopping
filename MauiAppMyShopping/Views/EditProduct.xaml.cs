19653a1f5d6a4b0c61e54ec7a211a202443290e0
Davi Bicudo <149242971+DaviBicudoo@users.noreply.github.com>
2025-03-24 10:34:32 -0300

    Update EditProduct.xaml.cs

--- MauiAppMyShopping/Views/EditProduct.xaml.cs
+++ MauiAppMyShopping/Views/EditProduct.xaml.cs
@@ -13,22 +13,22 @@ public partial class EditProduct : ContentPage
     {
 		try
 		{
-            Product anexedProduct = BindingContext as Product;
-
-            Product product = new Product
-            {
-                Id = anexedProduct.Id,
-                Description = productDescriptionTxt.Text,
-                Amount = Convert.ToInt16(productAmountTxt.Text),
-                Price = Convert.ToDouble(productUnitPrice.Text)
-            };
-
-            await App.Database.Update(product);
-
-            await DisplayAlert("Updated", "Success", "OK");
-
-            await Navigation.PopAsync();
-        }
+	            Product anexedProduct = BindingContext as Product; // Creates a variable (product type) which will be our Binding Context --> In other words, it will do the "ViewModel" function, linking the DB with the View
+	
+	            Product product = new Product
+	            {
+	                Id = anexedProduct.Id,
+	                Description = productDescriptionTxt.Text,
+	                Amount = Convert.ToInt16(productAmountTxt.Text),
+	                Price = Convert.ToDouble(productUnitPrice.Text)
+	            }; // Inserting the given values to a new instance of products
+	
+	            await App.Database.Update(product); // Update the product with new values
+	
+	            await DisplayAlert("Updated", "Success", "OK");
+	
+	            await Navigation.PopAsync();
+        	}
 		catch (Exception ex)
 		{
 			await DisplayAlert("OPS!", ex.Message, "OK");
